fb79c2c35de8990cfec1e43ab2e5b1d9459282bb
Marcel Savegnago <marcel.savegnago@gmail.com>
2021-02-02 23:54:00 -0300

    [ADD] base_repair_group

---
+++ base_repair_group/__manifest__.py
@@ -0,0 +1,20 @@
+# Copyright 2021 - TODAY, Escodoo
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'Base Repair Group',
+    'summary': """
+        Provides base access groups for the Repair App""",
+    'version': '12.0.1.0.0',
+    'license': 'AGPL-3',
+    'author': 'Escodoo,Odoo Community Association (OCA)',
+    'maintainers': ['marcelsavegnago'],
+    'images': ['static/description/banner.png'],
+    'website': 'https://github.com/oca/manufacture',
+    'depends': [
+    ],
+    'data': [
+    ],
+    'demo': [
+    ],
+}
---
+++ base_repair_group/readme/DESCRIPTION.rst
@@ -0,0 +1,5 @@
+This module provides a security group category 'Repair' with two new user
+groups:
+
+- 'User'
+- 'Full Access'.
